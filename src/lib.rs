da0b9dfd3d75fa16eb732ffca0468bf771cbb77a
Sascha Grunert <mail@saschagrunert.de>
2018-09-02 16:54:02 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+//! Yew routing extension
+
+#![deny(missing_docs)]
+
+extern crate failure;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate stdweb;
+extern crate yew;
+
+mod router;
+pub use router::{Request, Route, RouterAgent};
+
+#[macro_export]
+/// Convinience macro for route creation
+macro_rules! routes {
+    ($($x:tt => $y:expr,)*) => (
+        #[derive(Debug, PartialEq)]
+        /// Possible child components
+        pub enum RouterTarget {
+            $($x,)*
+        }
+
+        /// Convert a RouterTarget into a Route
+        impl<T> ::std::convert::Into<::yew_router::Route<T>> for RouterTarget where T: Default {
+            fn into(self) -> ::yew_router::Route<T> {
+                ::yew_router::Route {
+                    fragment: Some(
+                        match self {
+                            $(RouterTarget::$x => $y,)*
+                        }.into(),
+                    ),
+                    ..Default::default()
+                }
+            }
+        }
+
+        /// Convert a Route into a RouterTarget
+        impl ::std::convert::Into<RouterTarget> for ::yew_router::Route<()> {
+            fn into(self) -> RouterTarget {
+                match self.fragment {
+                    Some(f) => match f.as_str() {
+                        $($y => RouterTarget::$x,)*
+                        _ => RouterTarget::Error,
+                    },
+                    _ => RouterTarget::Error,
+                }
+            }
+        }
+    )
+}
