92a2d8e6faf902d6adb1a729e8798f7c0d2801b4
CircleCI <mail@saschagrunert.de>
2019-01-13 15:33:54 +0000

    Update documentation

--- doc/implementors/core/cmp/trait.PartialEq.js
+++ doc/implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["yew_router"] = [{text:"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"yew_router/struct.Route.html\" title=\"struct yew_router::Route\">Route</a>&lt;T&gt;",synthetic:false,types:["yew_router::router::Route"]},];
+implementors["yew_router"] = [{text:"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&lt;<a class=\"struct\" href=\"yew_router/struct.Route.html\" title=\"struct yew_router::Route\">Route</a>&lt;T&gt;&gt; for <a class=\"struct\" href=\"yew_router/struct.Route.html\" title=\"struct yew_router::Route\">Route</a>&lt;T&gt;",synthetic:false,types:["yew_router::router::Route"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ doc/source-files.js
@@ -0,0 +1,2 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["yew_router"] = {"name":"","dirs":[],"files":["lib.rs","router.rs"]};
