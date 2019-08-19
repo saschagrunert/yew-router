535603402ae014e8084a9d6cc10e828becffb68d
CircleCI <mail@saschagrunert.de>
2019-08-19 15:18:19 +0000

    Update documentation

---
+++ doc/implementors/std/panic/trait.RefUnwindSafe.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["yew_router"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html\" title=\"trait std::panic::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"yew_router/struct.Route.html\" title=\"struct yew_router::Route\">Route</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html\" title=\"trait std::panic::RefUnwindSafe\">RefUnwindSafe</a>,&nbsp;</span>",synthetic:true,types:["yew_router::router::Route"]},{text:"impl&lt;T&gt; !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html\" title=\"trait std::panic::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"yew_router/struct.RouterAgent.html\" title=\"struct yew_router::RouterAgent\">RouterAgent</a>&lt;T&gt;",synthetic:true,types:["yew_router::router::RouterAgent"]},{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html\" title=\"trait std::panic::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"enum\" href=\"yew_router/enum.Request.html\" title=\"enum yew_router::Request\">Request</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html\" title=\"trait std::panic::RefUnwindSafe\">RefUnwindSafe</a>,&nbsp;</span>",synthetic:true,types:["yew_router::router::Request"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
